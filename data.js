4dba7731a53a291f051ed4e353b8be6c811b0d0d
Diego-Emiliano <dieemigarruv@gmail.com>
2024-12-12 13:25:39 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,10 @@
+export data = [
+    { fecha: "1", nombre: "Estadística basica", url: "/" },
+    { fecha: "2", nombre: "Fundamentos de la Administración", url: "/" },
+    { fecha: "3", nombre: "Fundamentos de la Investigación", url: "/" },
+    { fecha: "4", nombre: "Organisación y gestión del tiempo", url: "/" },
+    { fecha: "5", nombre: "Trabajo en equipo", url: "/" },
+    { fecha: "6", nombre: "Toma de decisiones y comunicación asertiva", url: "/" },
+    { fecha: "6", nombre: "Creatividad y resolución de problemas", url: "/" },
+    { fecha: "6", nombre: "Contexto socioeconómico de México", url: "/" }
+];
