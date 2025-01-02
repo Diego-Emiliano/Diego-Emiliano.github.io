df2a30105eef17eb44d1959b4d06e8bf37b671d9
Diego-Emiliano <dieemigarruv@gmail.com>
2025-01-02 15:27:51 -0600

    Add orden in data.js

--- base/data.js
+++ base/data.js
@@ -1,3 +1,3 @@
 const data = [
-    { fecha: "07-12-2024", nombre: "Matemáticas básicas", descripcion: "Da un repaso por las raíces cuadradas, exponentes, orden de resolución y ejercicios de ello", categoria: "Matemáticas", url: "/matematicas-basicas" },
+    { orden: "1", fecha: "07-12-2024", nombre: "Matemáticas básicas", descripcion: "Da un repaso por las raíces cuadradas, exponentes, orden de resolución y ejercicios de ello", categoria: "Matemáticas", url: "/matematicas-basicas" },
 ];
